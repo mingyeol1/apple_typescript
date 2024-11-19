1f330abd5fd5d38e6ff439aa26628e3a4508434a
Mingyeol <alsruf741@gmail.com>
2024-11-19 17:52:03 +0900

    내일이 마지막

---
+++ a.ts
@@ -0,0 +1,1 @@
+export type obj = (a? :object) => void
--- index.ts
+++ index.ts
@@ -1,31 +1,20 @@
-function 함수(...a : number[]){
-    let 정답 : number = 0;
-    a.forEach(function(e){
-        if(정답 < e){
-            정답 = e;
-        }
-    })
-    
-    return console.log(정답);
+function 함수(...x : (string | number)[]) : [string[],number[]]{
+  
+  let str : string[] = [];
+  let num : number[] = []; 
+
+
+  x.forEach((a)=>{
+    if(typeof a === 'string'){
+      str.push(a)
+    }else{
+      num.push(a)
+    }
+  })
+
+  
+  
+  return  [str,num];
 }
 
-
-함수(1,2,3,4,5,6);
-
-
-function 함수2({user, comment, admin} : {user:string, comment: number[], admin:boolean}){
-    
-    
-    return console.log(user, comment, admin) 
-}
-
-
-함수2({ user : 'kim', comment : [3,5,4], admin : false })
-
-
-
-function 함수3({...a}){
-    console.log(a)
-}
-
-함수3([1,'123',true])
+console.log( 함수('123',1,3,'2'));
